587b793830c412a6b2f24de37ae8c0cc6a06dcb4
Lukas Hirsch <lukas.stag@gmail.com>
2024-06-21 23:37:09 +0200

    Switched to lazy-vim

--- .config/nvim/lua/lukas/init.lua
+++ .config/nvim/lua/lukas/init.lua
@@ -1,2 +1,25 @@
-require("lukas.remap")
 require("lukas.set")
+require("lukas.remap")
+require("lukas.lazy-init")
+
+local augroup = vim.api.nvim_create_augroup
+local LukasGroup = augroup('Lukas', {})
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    group = LukasGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>fl", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format() end, opts)
+    end
+})
---
+++ .config/nvim/lua/lukas/lazy/init.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+
+    { 'nvim-treesitter/playground' },
+    { "christoomey/vim-tmux-navigator" }
+}
