a200d44cecc597eb4f9683c151fd039ea614729c
Lukas Hirsch <lukas.stag@gmail.com>
2025-04-09 18:04:58 +0200

    Switch to blink.cmp

--- .config/nvim/lua/lukas/lazy/lsp.lua
+++ .config/nvim/lua/lukas/lazy/lsp.lua
@@ -1,113 +1,63 @@
 return {
   "neovim/nvim-lspconfig",
+
   dependencies = {
-    "williamboman/mason.nvim",
+    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+    { "williamboman/mason.nvim", opts = {} },
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "quangnguyen30192/cmp-nvim-ultisnips",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
+    -- "quangnguyen30192/cmp-nvim-ultisnips",
+
+    -- Status updates for LSP
     "j-hui/fidget.nvim",
   },
 
   config = function()
-    local cmp = require('cmp')
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities())
+    local original_capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
 
     require("fidget").setup({})
     require("mason").setup()
+
+
+    -- Define custom settings for specific servers
+    local servers = {
+      lua_ls = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT'
+        },
+        workspace = {
+          library = { vim.env.VIMRUNTIME },
+        },
+      }
+    }
+
     require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "gopls",
-      },
+      ensure_installed = servers,
+
       handlers = {
         function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities
-          }
-        end,
-
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup {
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                runtime = { version = "Lua 5.1" },
-                diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                }
-              }
-            }
-          }
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+          require("lspconfig")[server_name].setup(server)
         end,
       }
     })
 
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-    local defaultSources = cmp.config.sources({
-      { name = 'copilot' },
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnip users.
-      { name = 'path' },
-      -- { name = 'buffer' },
-    })
-
-    cmp.setup({
-      preselect = 'none',
-      completion = {
-        completeopt = 'menu,menuone,noinsert,noselect'
-      },
-
-      sources = defaultSources,
-
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-
-      mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      })
-    })
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = "tex",
-      callback = function()
-        local sources = defaultSources
-        sources[#sources + 1] = { name = 'ultisnips', group_index = 3 }
-        cmp.setup.buffer {
-          sources = sources
-        }
-      end
-    })
-
-
     vim.diagnostic.config({
       -- update_in_insert = true,
+      severity_sort = true,
       float = {
-        focusable = false,
-        style = "minimal",
         border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
+        source = "always"
+      },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅚 ",
+          [vim.diagnostic.severity.WARN] = "󰀪 ",
+          [vim.diagnostic.severity.INFO] = "󰋽 ",
+          [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
       },
     })
   end
