73c0eece7a29fecf4a67a963b1c124c9a58d4aa3
Lukas Hirsch <lukas.stag@gmail.com>
2025-11-19 19:08:28 +0100

    disable follow cursor

--- .config/nvim/lua/lukas/lazy/typst-preview.lua
+++ .config/nvim/lua/lukas/lazy/typst-preview.lua
@@ -2,5 +2,7 @@ return {
   'chomosuke/typst-preview.nvim',
   lazy = false, -- or ft = 'typst'
   version = '1.*',
-  opts = {},    -- lazy.nvim will implicitly calls `setup {}`
+  opts = {
+    follow_cursor = false
+  },    -- lazy.nvim will implicitly calls `setup {}`
 }
