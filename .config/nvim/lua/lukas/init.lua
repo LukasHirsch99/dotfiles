e2e9835d404d702fbf50bb08f824230a3d13320c
Lukas Hirsch <lukas.stag@gmail.com>
2025-11-19 19:07:58 +0100

    make & typst-preview shortcut, makeprg python

--- .config/nvim/lua/lukas/init.lua
+++ .config/nvim/lua/lukas/init.lua
@@ -2,3 +2,10 @@ require("lukas.set")
 require("lukas.keymaps")
 require("lukas.lazy-init")
 require("lukas.lsp")
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "python",
+    callback = function()
+        vim.opt_local.makeprg = "python3 %:S"
+    end,
+})
--- .config/nvim/lua/lukas/keymaps.lua
+++ .config/nvim/lua/lukas/keymaps.lua
@@ -28,3 +28,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>tp", "<cmd>TypstPreviewToggle<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>m", "<cmd>w<CR> <bar> <cmd>make<CR>", { silent = true })
